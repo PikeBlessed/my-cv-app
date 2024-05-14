d73aed13a09e3636b58d86603b5cb281db911999
PikeBlessed <depaulaoctavio04@gmail.com>
2024-05-14 15:05:06 -0300

    Contact form added

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from experience import stream_data, toggle_newsletter_stats, toggle_button
 
 
 #Presentation
-col_1, col_2 = st.columns([0.8, 0.3])
+col_1, col_2 = st.columns([0.8, 0.2])
 with col_1: 
     st.title('Octavio de Paula')
     st.subheader('Marketing Specialist - Data Science Student')
@@ -57,10 +57,64 @@ if experience_button:
 #Projects
 projects = '''
     - Data science and MLOPs: I have made several projects about Data, from simple things such as Data Analysis, small ML models, ETL process to complete data science pipeline (ETL, EDA, modeling, MLOPs, etc).
-        - ML models: 
+        - ML models: My [best project](https://github.com/PikeBlessed/deploy-project-datascience) about MLOPs, that was to PHNAN. In this project I've applied a complete data science pipeline, API(FastAPI), differents ML models, Docker, etc.
+        - Data Analysis: [Project](https://deepnote.com/workspace/proyecto-pandas-y-numpy-9f6dc92a-c802-474f-b7a1-01e51b6fc675/project/Octavios-Untitled-project-29dbf950-c5bc-4211-afb5-bd91a042da55/notebook/proyecto_data_science-d6f691c0cad446d29fbc07a32780a1a6) 
+        - IA Photographic App (in progress): An app about a chatbot integred with IA to analyze photos and bot can act like profesional.  
 '''
 
 projects_button = toggle_button('Deploy Projects')
 
 if projects_button:
     st.write(stream_data(projects))
+
+form_action = f"https://formsubmit.co/depaulaoctavio04@gmail.com"
+
+with st.expander('Contact with me'):
+    with st.container(border=True, height=470):
+        name = st.text_input("Name",)
+        email = st.text_input("Email")
+        subject = st.text_input("Subject")
+        message = st.text_area("Message")
+
+
+        form_fields = {
+            "name": name,
+            "email": email,
+            "subject": subject,
+            "message": message
+        }
+
+        form_html = f"""
+        <form action="{form_action}" method="POST">
+            <input type="hidden" name="name" value="{name}" required>
+            <input type="hidden" name="email" value="{email}" required>
+            <input type="hidden" name="message" value="{message}" required>
+            <input type="hidden" name="_subject" value="{subject}" required>
+            <input type="hidden" name="_captcha" value="false">
+            <input type="hidden" name="_template" value="table">
+            <input type="submit" value="Send">
+        </form>
+        """
+
+        code = '''
+            <style>
+                input[type="submit"] {
+                    background-color: #FFA07A;
+                    color: #000000;
+                    padding: 5px 10px;
+                    border-style: solid;
+                    border-width: 0px; 
+                    border-color: #87CEEB;
+                    border-radius: 5px;
+                    cursor: pointer;
+                    width: 100%;
+                }
+
+                .refill {
+                    border-bottom: 1px solid #ccc;
+                    margin-top: 10px;
+                }
+            </style>
+        '''
+        st.html(code)
+        st.markdown(form_html, unsafe_allow_html=True)
