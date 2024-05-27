79fb537b64fc7c4e6eb60a85e33edf015d860224
PikeBlessed <depaulaoctavio04@gmail.com>
2024-05-27 10:52:52 -0300

    Modifyng secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ def projects_button():
 projects_button()
 
 #Capabilities connected with SQL database
-conn = st.experimental_connection(name="postgresqll", type="sql", kwargs=st.secrets.connections.postgresqll)
+conn = st.connection(name="postgresqll", type="sql", kwargs=st.secrets.connections.postgresqll)
 
 
 capabilities = st.radio('###### Select what you want see', ['Hard skills', 'Technologies', 'Soft skills'], horizontal=True, index=None)
